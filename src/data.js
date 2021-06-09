f61da59a3d3d4828faf5d697367c3956f7fd7cd5
Joó Norbert <joonorbi12@gmail.com>
2021-06-09 13:34:32 +0200

    added FAQ data

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const fAQData = [
+  {
+    id: 0,
+    question: 'How many team members can I invite?',
+    answer:
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+    toggled: false,
+  },
+  {
+    id: 1,
+    question: 'What is the maximum file upload size?',
+    answer:
+      'No morse than 2GB. All files in your account must fit your allotted storage space.',
+    toggled: false,
+  },
+  {
+    id: 2,
+    question: 'How do I reset my password?',
+    answer:
+      ' Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
+    toggled: false,
+  },
+  {
+    id: 3,
+    question: 'Can I cancel my subscription?',
+    answer:
+      'Yes! Send us a message and we’ll process your request no questions asked.',
+    toggled: false,
+  },
+  {
+    id: 4,
+    question: 'Do you provide additional support?',
+    answer:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+    toggled: false,
+  },
+];
